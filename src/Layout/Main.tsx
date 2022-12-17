b50ee5bec90b72a99b62c6c3feb1c40b1353ceca
unknown <textile.zero@gmail.com>
2022-12-18 05:36:49 +0600

    flowbite adn firebase configured

--- src/Layout/Main.tsx
+++ src/Layout/Main.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Navbar from '../components/Shared/Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
 import Footer from '../components/Shared/Footer/Footer';
+import Nav from '../components/Shared/Navbar/Nav';
 
 const Main = () => {
     return (
         <div>
-            <Navbar></Navbar>
+            <Nav></Nav>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
